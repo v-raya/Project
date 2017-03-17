fd1931886474a82dd8a0037ecdd22b63cd8cd990
Keith Lauher <klauher@cambriasolutions.com>
2017-03-17 09:40:34 -0700

    add unit tests for facility index, show, and refresh

--- spec/features/show_facility_test_spec.rb
+++ spec/features/show_facility_test_spec.rb
@@ -2,10 +2,23 @@ require 'rails_helper'
 require 'vcr'
 
 RSpec.feature 'Facilities', js: true do
-  scenario 'List of facilities' do
-    visit '/facilities'
-    expect(page).to have_text('MARCH, AMY CFH')
-  end
+    scenario 'List of facilities' do
+        visit '/facilities'
+        expect(page).to have_text('MARCH, AMY CFH')
+    end
+    scenario 'click into facility show from list of facilities' do 
+        visit '/facilities'
+        click_link("MARCH, AMY CFH")
+        expect(page).to have_text('MARCH, AMY CFH')
+    end
+
+    scenario 'refresh facility' do 
+        visit '/facilities'
+        click_link("MARCH, AMY CFH")
+        expect(page).to have_text('MARCH, AMY CFH')
+        page.evaluate_script("window.location.reload()")
+        expect(page).to have_text('MARCH, AMY CFH')
+    end
 end
 
 
