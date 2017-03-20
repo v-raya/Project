425e8247570f7188e6026ad94050a04b5c6c0e56
Keith Lauher <klauher@cambriasolutions.com>
2017-03-20 15:27:03 -0700

    port over lavanya implementation of elastic search POC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-    root 'pages#index'
-    resources :facilities,  only:[:index, :show]
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  resources :facilities,  only:[:index, :show] do
+    collection { get :search }
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
