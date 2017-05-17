af7b3386504fd44f63cf87afa021af5ac029737e
Lavanya Reddy <lreddy@cambriasolutions.com>
2017-05-17 10:26:32 -0700

    fixed tests to test params_to_query_hash method, updated county and type column names to match field names

--- app/javascript/search/search.js
+++ app/javascript/search/search.js
@@ -28,8 +28,8 @@ export default class Search extends React.Component {
   addSearchInput (DataSearch) {
     var query = DataSearch.split(",");
     var params = {
-     county : [query[0]],
-     type : [query[1]],
+     fac_co_nbr : [query[0]],
+     fac_type : [query[1]],
      fac_nbr: [query[2]],
      fac_name : [query[3]],
      fac_addr : ['']
