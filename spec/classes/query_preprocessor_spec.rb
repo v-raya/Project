35651b03d135d0b8d30fdfa6650d8b152e34cfec
Lavanya Reddy <lreddy@cambriasolutions.com>
2017-05-17 10:26:32 -0700

    fixed tests to test params_to_query_hash method, updated county and type column names to match field names

--- spec/classes/query_preprocessor_spec.rb
+++ spec/classes/query_preprocessor_spec.rb
@@ -6,32 +6,24 @@ describe QueryPreprocessor do
   describe '#search_query' do
     context 'with more than 1 non empty arrays as values' do
       it 'builds query with single element arrays as values' do
-        expected_output = {query: {bool: {should: [{bool: {must: [{match: {county: '01'}}, {match: {type: '02'}}, {match: {fac_name: 'home'}}]}}]}}}
-
-        unprocessed_params = {'county': ['01'], 'type': ['02'], 'fac_nbr': [''], 'fac_name': ['home'], 'fac_addr': ['']}
-        output = QueryPreprocessor.params_to_elasticsearch_query(unprocessed_params)
+        expected_output = [{:county=>'01', :type=>'02', :fac_name=>'home'}]
+        params = {'county': ['01'], 'type': ['02'], 'fac_nbr': [''], 'fac_name': ['home'], 'fac_addr': ['']}
+        output = QueryPreprocessor.params_to_query_hash(params)
 
         expect(output).to eq(expected_output)
       end
 
       it 'builds query with multiple element arrays as values' do
-        expected_output = {query:
-                               {bool:
-                                    {should:
-                                         [{bool: {must: [{match: {county: '01'}}, {match: {type: '2'}}, {match: {fac_nbr: '80'}}]}},
-                                          {bool: {must: [{match: {county: '01'}}, {match: {type: '2'}}, {match: {fac_nbr: '90'}}]}},
-                                          {bool: {must: [{match: {county: '01'}}, {match: {type: '4'}}, {match: {fac_nbr: '80'}}]}},
-                                          {bool: {must: [{match: {county: '01'}}, {match: {type: '4'}}, {match: {fac_nbr: '90'}}]}},
-                                          {bool: {must: [{match: {county: '02'}}, {match: {type: '2'}}, {match: {fac_nbr: '80'}}]}},
-                                          {bool: {must: [{match: {county: '02'}}, {match: {type: '2'}}, {match: {fac_nbr: '90'}}]}},
-                                          {bool: {must: [{match: {county: '02'}}, {match: {type: '4'}}, {match: {fac_nbr: '80'}}]}},
-                                          {bool: {must: [{match: {county: '02'}}, {match: {type: '4'}}, {match: {fac_nbr: '90'}}]}}]
-                                    }
-                               }
-        }
-
-        unprocessed_params = {'county': ['01', '02'], 'type': ['2', '4'], 'fac_nbr': ['80', '90']}
-        output = QueryPreprocessor.params_to_elasticsearch_query(unprocessed_params)
+        expected_output =[{:county=>'01', :type=>'2', :fac_nbr=>'80'},
+                          {:county=>'01', :type=>'2', :fac_nbr=>'90'},
+                          {:county=>'01', :type=>'4', :fac_nbr=>'80'},
+                          {:county=>'01', :type=>'4', :fac_nbr=>'90'},
+                          {:county=>'02', :type=>'2', :fac_nbr=>'80'},
+                          {:county=>'02', :type=>'2', :fac_nbr=>'90'},
+                          {:county=>'02', :type=>'4', :fac_nbr=>'80'},
+                          {:county=>'02', :type=>'4', :fac_nbr=>'90'}]
+        params = {'county': ['01', '02'], 'type': ['2', '4'], 'fac_nbr': ['80', '90']}
+        output = QueryPreprocessor.params_to_query_hash(params)
 
         expect(output).to eq(expected_output)
       end
@@ -39,10 +31,9 @@ describe QueryPreprocessor do
 
     context 'with only one multi element array as value' do
       it 'builds query with one multiple element array as value' do
-        expected_output = {query: {bool: {should: [{bool: {must: [{match: {county: '01'}}]}}, {bool: {must: [{match: {county: '02'}}]}},
-                                                   {bool: {must: [{match: {county: '03'}}]}}]}}}
-        unprocessed_params = {'county': ['01', '02', '03']}
-        output = QueryPreprocessor.params_to_elasticsearch_query(unprocessed_params)
+        expected_output = [{:county=>'01'}, {:county=>'02'}, {:county=>'03'}]
+        params = {'county': ['01', '02', '03']}
+        output = QueryPreprocessor.params_to_query_hash(params)
 
         expect(output).to eq(expected_output)
       end
