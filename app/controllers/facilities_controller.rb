9347abac54d8fb0dbbac491fbfe39477ad7f5c0b
Lavanya Reddy <lreddy@cambriasolutions.com>
2017-05-18 11:35:10 -0700

    fixed CSRF token validation

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -2,8 +2,6 @@ require 'hypernova'
 
 class FacilitiesController < CalsBaseController
   around_action :hypernova_render_support
-  skip_before_action :verify_authenticity_token, only: [:search]
-
   include Response
 
   def index
