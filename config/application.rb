61506c4c476ac5f8018493d0719158dfec2ea9e9
Ratnesh Raval <ratneshraval@gmail.com>
2017-05-19 11:27:38 -0700

    Log output to STDOUT (#66)

--- config/application.rb
+++ config/application.rb
@@ -4,14 +4,12 @@ require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
-# require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 #require "sprockets/railtie"
 require "rails/test_unit/railtie"
-#require "active_resource/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +20,9 @@ module Cals
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    logger = ActiveSupport::Logger.new(STDOUT)
+    config.log_level = :debug
+
   end
 end
