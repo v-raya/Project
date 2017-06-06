9ab58157a43ef5f5e4cb307024a7c31d67027ce5
Ratnesh Raval <rraval@cambriasolutions.com>
2017-06-05 22:32:09 -0700

    missed update to webpacker test config to generate karma reprots

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -3,4 +3,21 @@
 const merge = require('webpack-merge')
 const sharedConfig = require('./shared.js')
 
-module.exports = merge(sharedConfig, {})
+module.exports = merge(sharedConfig, {
+  devtool: 'sourcemap',
+  module: {
+    rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'istanbul-instrumenter-loader',
+      query: {
+        esModules: true
+      }
+    }
+    ]
+  }
+})
+
+
+
