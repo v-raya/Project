f742302aaa7ba917ebb7215789dc2e9b540d0cf2
Ratnesh Raval <rraval@cambriasolutions.com>
2017-06-19 15:11:18 -0700

    removing lint errors

--- app/javascript/search/search.js
+++ app/javascript/search/search.js
@@ -58,50 +58,22 @@ export default class Search extends React.Component {
 
     this.state.inputData = DataSearch
 
-    //call http request function with arguments
-    var url = urlPrefixHelper('/facilities/search');
+    // call http request function with arguments
+    var url = urlPrefixHelper('/facilities/search')
     fetchRequest(url, 'POST', params).then(
       response => response.json())
     .then((response) => {
-      return this.setState ({
+      return this.setState({
         searchData: response.facilities
       })
     })
     .catch(error => {
       console.log(error)
-      return this.setState ({
+      return this.setState({
         searchData: [],
         fromResponse: true
       })
     })
-
-    // fetch('/facilities/search', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Accept': 'application/json',
-    //     'X-CSRF-Token': this.getCsrfToken('csrf-token'),
-    //     'X-CSRF-param': this.getCsrfToken('csrf-param')
-    //   },
-    //   credentials: 'same-origin',
-    //   body: JSON.stringify({
-    //     params
-    //   })
-    // })
-    // .then(
-    //   response => response.json())
-    // .then((response) => {
-    //   return this.setState({
-    //     searchData: response.facilities
-    //   })
-    // })
-    // .catch(error => {
-    //   console.log(error)
-    //   return this.setState({
-    //     searchData: [],
-    //     fromResponse: true
-    //   })
-    // })
   }
 
   render () {
