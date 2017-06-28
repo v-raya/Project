5614bbd1258bdea1ca3c35df71479b8cccb59466
anuroop15 <anuroop.buggaveeti15@gmail.com>
2017-06-28 13:53:33 -0700

    fix resident border (#106)

--- app/javascript/forms/residenceCardsMain.jsx
+++ app/javascript/forms/residenceCardsMain.jsx
@@ -35,13 +35,13 @@ export default class ResidenceCards extends React.Component {
     const {formData} = this.state
     return (
       <div className='residence_cards'>
-        <div id="residentAddress" onClick={this.toggleOnFocus.bind(this, 'residentAddress')} className={(this.state.isFocused['residentAddress']) + ' '  + "card name-section double-gap-top"}>
+        <div id="residentAddress" onClick={this.toggleOnFocus.bind(this, 'residentAddress')} className={(this.state.isFocused['residentAddress']) + ' '  + "card resident-section double-gap-top"}>
           <div className='card-header'>
             <span>Address</span>
           </div>
           <AddressCard {...this.props} />
         </div>
-        <div id="aboutResidence" onClick={this.toggleOnFocus.bind(this, 'aboutResidence')} className={(this.state.isFocused['aboutResidence']) + ' '  + "card name-section double-gap-top"}>
+        <div id="aboutResidence" onClick={this.toggleOnFocus.bind(this, 'aboutResidence')} className={(this.state.isFocused['aboutResidence']) + ' '  + "card about-resident-section double-gap-top"}>
           <div className='card-header'>
             <span>About This Residence</span>
           </div>
