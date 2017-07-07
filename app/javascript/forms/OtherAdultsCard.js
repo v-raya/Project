f5829e2eefdee442e7602edc19f7c5491a92d4a9
Keith Lauher <klauher@cambriasolutions.com>
2017-07-07 15:30:58 -0700

    oopsies tpyo

--- app/javascript/forms/OtherAdultsCard.js
+++ app/javascript/forms/OtherAdultsCard.js
@@ -1,4 +1,4 @@
-import React from 'React'
+import React from 'react'
 import {OtherAdultsCardField} from '../common/OtherAdultsCardField'
 
 export default class OtherAdultsCard extends React.Component {
