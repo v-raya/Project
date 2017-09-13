5e61c6b487e77f39a306f15a9b2f20d9682e487c
anuroop15 <anuroop.buggaveeti15@gmail.com>
2017-09-12 20:04:23 -0700

    fix undefine issue on date (#172)

--- app/javascript/helpers/commonHelper.jsx
+++ app/javascript/helpers/commonHelper.jsx
@@ -18,20 +18,24 @@ export const removeLegalNameType = (nameTypes) => {
 export const FormatDoBForPersistance = (dateString) => {
   dateString = dateString.replace(/_/gi, '')
   let persistantDateString
-  if (dateString.length === 10) {
+  if (dateString && dateString.length === 10) {
     const dateStringArray = dateString ? dateString.split('/', 3) : []
     // we need the user input order of mm-dd-yyy to  be yyyy-mm-dd
     persistantDateString = ([dateStringArray[2], dateStringArray[0], dateStringArray[1]]).join('-')
+  } else {
+    dateString = ''
   }
   return persistantDateString || dateString.replace(/\//gi, '-')
 }
 
 export const FormateDobForDisplay = (dateString) => {
   let persistantDateString
-  if (dateString.length === 10) {
+  if (dateString && dateString.length === 10) {
     const dateStringArray = dateString ? dateString.split('-', 3) : []
     // we need the user input order of mm-dd-yyy to  be yyyy-mm-dd
     persistantDateString = ([dateStringArray[1], dateStringArray[2], dateStringArray[0]]).join('/')
+  } else {
+    dateString = ''
   }
   return persistantDateString || dateString.replace(/-/gi, '/')
 }
