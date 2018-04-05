5a27d807a8419bdb68372f2831beff86d0bca34f
Ratnesh Raval <ratneshraval@gmail.com>
2018-04-05 13:04:46 -0700

    Remove file loader (#421)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,7 +3,4 @@ const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 environment.loaders.append('erb', erb)
 
-const fileLoader = require('./loaders/file')
-environment.loaders.append('file', fileLoader)
-
 module.exports = environment
--- config/webpack/loaders/file.js
+++
@@ -1,8 +0,0 @@
-const { environment } = require('@rails/webpacker')
-
-const publicPath = environment.config.output.publicPath
-const fileLoader = environment.loaders.get('file')
-
-fileLoader.use[0].options.publicPath = process.env.RAILS_RELATIVE_URL_ROOT ? process.env.RAILS_RELATIVE_URL_ROOT + publicPath : publicPath
-
-module.exports = fileLoader
