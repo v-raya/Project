bca26bb3ba754bf5c222f543775ca8a69bfc093d
anuroop15 <anuroop.buggaveeti15@gmail.com>
2018-05-16 17:47:28 -0700

    wrap list inside container (#481)

--- app/javascript/rfa_forms/rfa01a_list/applicationsTable.jsx
+++ app/javascript/rfa_forms/rfa01a_list/applicationsTable.jsx
@@ -7,39 +7,41 @@ export default class ApplicationsTable extends React.Component {
   render () {
     const applicationsList = Immutable.fromJS(this.props.applications)
     return (
-      <div className='rfa01a-list table-responsive'>
-        <h3>Existing RFA Application</h3>
-        <table className='table table-hover'>
-          <thead>
-            <tr>
-              <th scope="col">Facility ID</th>
-              <th scope="col">Facility / Family Name</th>
-              <th scope="col">Phone</th>
-              <th scope="col">Address</th>
-              <th scope="col">City</th>
-              <th scope="col">Zip</th>
-              <th scope="col">Status</th>
-              <th scope="col">Received Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              applicationsList.map((application, index) => {
-                return (
-                  <ApplicationsListRow
-                    key={index}
-                    familyName={application.get('resource_family_name')}
-                    facilityId={application.get('id')}
-                    applicantsInfo={application.get('applicants')}
-                    applicationStatus={application.get('status')}
-                    applicationReceivedDate={application.get('receivedDate')}
-                    applicationAddress={application.getIn(['residence', 'addresses'])}
-                  />
-                )
-              })
-            }
-          </tbody>
-        </table>
+      <div className='container'>
+        <div className='rfa01a-list table-responsive'>
+          <h3>Existing RFA Application</h3>
+          <table className='table table-hover'>
+            <thead>
+              <tr>
+                <th scope="col">Facility ID</th>
+                <th scope="col">Facility / Family Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Address</th>
+                <th scope="col">City</th>
+                <th scope="col">Zip</th>
+                <th scope="col">Status</th>
+                <th scope="col">Received Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                applicationsList.map((application, index) => {
+                  return (
+                    <ApplicationsListRow
+                      key={index}
+                      familyName={application.get('resource_family_name')}
+                      facilityId={application.get('id')}
+                      applicantsInfo={application.get('applicants')}
+                      applicationStatus={application.get('status')}
+                      applicationReceivedDate={application.get('receivedDate')}
+                      applicationAddress={application.getIn(['residence', 'addresses'])}
+                    />
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
