97714a223345604d4a349107133091291da1e95a
anuroop15 <anuroop.buggaveeti15@gmail.com>
2018-06-26 17:30:39 -0700

    fix acceptance tests (#547)

--- spec/acceptance/rfa_forms/rfa_tracking_spec.rb
+++ spec/acceptance/rfa_forms/rfa_tracking_spec.rb
@@ -5,15 +5,27 @@ require 'vcr'
 require 'faker'
 
 RSpec.feature 'RFATracking', js: true, inaccessible: true do
-  scenario 'visit tracking page from dashboard', set_auth_header: true do
+
+  before do
     visit root_path
-    first('.tracking').click_link
+    click_button 'Create RFA Application'
+    fill_in('applicants[0].first_name', with: 'James', match: :prefer_exact)
+    fill_in('applicants[0].last_name', with: '123Monteo', match: :prefer_exact)
+    click_button 'Save Progress'
+    fill_in('relationship_to_applicants0adult0relationship_to_applicant_freeform', with: 'Friend')
+    find('label[for="other_adults[0].is_residing_in_hometrue"]').click
+    fill_in('other_adults[0].first_name', with: Faker::Name.first_name, match: :prefer_exact)
+    click_button 'Save Progress'
+    click_link 'RFA Application list'
+    expect(page).to have_content('123Monteo, James')
+    page.find("a", :text => '123Monteo, James', match: :first).find(:xpath,"..//..", match: :first).find("a", text: 'tracking').click
+  end
+     
+  scenario 'visit tracking page from dashboard', set_auth_header: true do
     expect(page).to have_content '-RFA Application'
   end
 
   scenario 'visit tracking page from dashboard ande save', set_auth_header: true do
-    visit root_path
-    first('.tracking').click_link
     expect(page).to have_button 'Edit Checklist'
     click_button 'Edit Checklist'
     expect(page).to have_button 'Save'
@@ -21,13 +33,10 @@ RSpec.feature 'RFATracking', js: true, inaccessible: true do
   end
 
   scenario 'visit tracking page from dashboard and Edit', set_auth_header: true do
-    visit root_path
-    first('.tracking').click_link
     expect(page).to have_button 'Edit Checklist'
     click_button 'Edit Checklist'
     expect(page).to have_button 'Save'
     expect(page).to have_button 'Cancel'
-
     fill_in('familyEditRecievedDate0', with: '12/11/1987', match: :prefer_exact)
     fill_in('familyEditNotes0', with: 'testing', match: :prefer_exact)
     fill_in('taskAndTrainingEditCompletedDate0', with: '12/12/1987', match: :prefer_exact)
@@ -44,10 +53,8 @@ RSpec.feature 'RFATracking', js: true, inaccessible: true do
     expect(page).to have_content '12/14/1987'
     expect(page).to have_content 'testing'
   end
-
+  
   scenario 'visit tracking page from dashboard and cancel', set_auth_header: true do
-    visit root_path
-    first('.tracking').click_link
     expect(page).to have_button 'Edit Checklist'
     click_button 'Edit Checklist'
     expect(page).to have_button 'Save'
@@ -94,9 +101,7 @@ RSpec.feature 'RFATracking', js: true, inaccessible: true do
     expect(page).to have_content '12/14/1987'
     expect(page).to have_content 'testing'
   end
-  scenario 'visit tracking page from dashboard and Edit Other Adults Row', set_auth_header: true do
-    visit root_path
-    first('.tracking').click_link
+  scenario 'visit tracking page from dashboard and Edit Other Adults Row', set_auth_header: true do 
     expect(page).to have_button 'Edit Checklist'
     click_button 'Edit Checklist'
     expect(page).to have_button 'Save'
