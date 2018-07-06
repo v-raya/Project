a0656e373ebd196ff73f3d4db5d80f3a927baa3a
sunil biradar <sunilb0575@gmail.com>
2018-07-06 11:47:54 -0700

    SEAR-279 Fix incorrect display of address

--- app/javascript/search/searchList.js
+++ app/javascript/search/searchList.js
@@ -1,50 +1,101 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ReactTable from 'react-table'
 import {urlPrefixHelper} from '../helpers/url_prefix_helper.js.erb'
 import {Link} from 'react-router-dom'
 import {checkForNA, checkForObjectAndValue, checkNullOrEmptyValue, respectiveFullAddressOrNA, respectiveNumberOrNA,
   primaryPhoneRelation, alternativePhoneRelation, physicalAddressType} from './common/commonUtils'
 
+const searchListColumns = [
+  {
+    Header: 'Facility Name',
+    minWidth: 70,
+    className: 'facility_name',
+    id: 'facilityName',
+    accessor: data => (<Link to={urlPrefixHelper('/facilities/' + data.id)}>{checkNullOrEmptyValue(data.name)}</Link>)
+  },
+  {
+    Header: 'Facility ID / Approval #',
+    className: 'facility_id',
+    minWidth: 70,
+    accessor: 'license_number'
+  },
+  {
+    Header: 'Facility Type / Facility Source',
+    id: 'facilityTypeSource',
+    className: 'facility_type_source',
+    minWidth: 100,
+    accessor: data => (`${checkForNA(data.type)} / ${checkNullOrEmptyValue(data.facility_source)}`)
+  },
+  {
+    Header: 'License Status',
+    id: 'licenseeStatus',
+    className: 'license_status',
+    minWidth: 80,
+    accessor: data => checkForNA(data.status)
+  },
+  {
+    Header: 'Licensee Name',
+    className: 'licensee_name',
+    minWidth: 80,
+    accessor: 'licensee_name'
+  },
+  {
+    Header: 'Facility Address',
+    id: 'facilityAddress',
+    className: 'facility_address',
+    minWidth: 150,
+    accessor: data => respectiveFullAddressOrNA(data.addresses, physicalAddressType)
+  },
+  {
+    Header: 'County',
+    id: 'countyName',
+    className: 'county_name',
+    minWidth: 70,
+    accessor: data => checkForNA(data.county)
+  },
+  {
+    Header: 'Phone Number',
+    minWidth: 70,
+    id: 'phoneNumber',
+    className: 'phone_number',
+    accessor: data => respectiveNumberOrNA(data.phones, primaryPhoneRelation)
+  },
+  {
+    Header: 'Facility Email',
+    id: 'facilityEmail',
+    className: 'facility_email',
+    accessor: data => checkNullOrEmptyValue(data.email_address)
+  },
+  {
+    Header: 'Approval / Licensing Worker',
+    id: 'licensingWorker',
+    className: 'licensing_worker',
+    accessor: data => checkForObjectAndValue(data.assigned_worker, 'full_name')
+  },
+  {
+    Header: 'Alternative Number',
+    id: 'alternativeNumber',
+    className: 'alternative_number',
+    accessor: data => respectiveNumberOrNA(data.phones, alternativePhoneRelation)
+  }
+]
+
 export const SearchList = ({
   searchResults
 }) => (
   <div className='main_table'>
-    <table className='table'>
-      {searchResults.length > 0 && <thead>
-        <tr>
-          <th>Facility Name </th>
-          <th>Facility ID / Approval #</th>
-          <th>Facility Type / Facility Source</th>
-          <th>License Status</th>
-          <th>Licensee Name</th>
-          <th>Facility Address</th>
-          <th>County</th>
-          <th>Phone Number</th>
-          <th>Facility Email</th>
-          <th>Approval / Licensing Worker</th>
-          <th>Alternative Number</th>
-        </tr>
-      </thead>}
-      <tbody>
-        {searchResults.map((result, index) => {
-          return (
-            <tr key={index}>
-              <td><Link to={urlPrefixHelper('/facilities/' + result.id)}>{checkNullOrEmptyValue(result.name)}</Link></td>
-              <td>{result.license_number}</td>
-              <td id={'facilities[' + index + '].' + 'type'}>{checkForNA(result.type) + ' / ' + checkNullOrEmptyValue(result.facility_source)}</td>
-              <td>{checkForNA(result.status)}</td>
-              <td id={'facilities[' + index + '].' + 'licensee_name'}>{result.licensee_name}</td>
-              <td id={'facilities[' + index + '].' + 'physical_address'}>{respectiveFullAddressOrNA(result.addresses, physicalAddressType)}</td>
-              <td id={'facilities[' + index + '].' + 'county'}>{checkForNA(result.county)}</td>
-              <td id={'facilities[' + index + '].' + 'primary_phone'}>{respectiveNumberOrNA(result.phones, primaryPhoneRelation)}</td>
-              <td>{checkNullOrEmptyValue(result.email_address)}</td>
-              <td>{checkForObjectAndValue(result.assigned_worker, 'full_name')}</td>
-              <td>{respectiveNumberOrNA(result.phones, alternativePhoneRelation)}</td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+    {
+      searchResults.length > 0 &&
+      <ReactTable
+        data={searchResults}
+        columns={searchListColumns}
+        pageSize={searchResults.length}
+        sortable={false}
+        resizable={true}
+        showPagination={false}
+      />
+    }
   </div>
 )
 
--- test/javascript/search/searchList_tests.js
+++ test/javascript/search/searchList_tests.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import SearchList from 'search/searchList'
-import { shallow } from 'enzyme'
+import { BrowserRouter } from 'react-router-dom'
+import { mount } from 'enzyme'
 
 describe('Render search list and each method', function () {
   const indexValue = '0'
   const props = {
     searchResults: [{
       assigned_worker: {
-        id: '1',
-        value: 'Kari Gutierrez'
+        'phones': [
+          {
+            'relation': 'primary',
+            'number': '9165672124'
+          }
+        ],
+        'email': null,
+        'full_name': 'Kari Gutierrez',
+        'id': null
       },
       county: {
         id: '19',
@@ -27,7 +35,7 @@ describe('Render search list and each method', function () {
       fac_mail_state: 'CA',
       fac_mail_street_addr: '767 GLEN EAGLES DRIVE',
       fac_mail_zip_code: '95350',
-      name: "DEPUTY DOG'S GROUP HOME",
+      name: 'DEPUTY DOG GROUP HOME',
       licensee_name: 'Ananya Nandi',
       license_number: 193600008,
       fac_orig_appl_rec_date: '1983-02-02',
@@ -67,6 +75,12 @@ describe('Render search list and each method', function () {
         'relation': 'primary',
         'type': 'Cell',
         'number': '8317632735'
+      },
+      {
+        'id': null,
+        'relation': 'alternate',
+        'type': 'Cell',
+        'number': '1234567891'
       }],
       last_visit_reason: "Renewal (Fac.'s w/Expir.)",
       status: {
@@ -86,45 +100,65 @@ describe('Render search list and each method', function () {
       facility_source: null
     }]
   }
-  let searchListComp = shallow(< SearchList { ...props} />)
+  let searchListComp = mount(<BrowserRouter><SearchList searchResults={props.searchResults} /></BrowserRouter>)
 
   it('Verify Search component render', () => {
     expect(searchListComp.length).toBe(1)
   })
 
-  it('Verify facility county renderd', () => {
-    expect(searchListComp.findWhere(n => n.props().id === 'facilities[0].county').props().children).toBe('Los Angeles')
+  it('Verify facility name is rendered', () => {
+    expect(searchListComp.find('.-odd .facility_name').hostNodes().props().children.props.children).toBe('DEPUTY DOG GROUP HOME')
   })
 
-  it('Verify facility county is N/A', () => {
-    expect(searchListComp.findWhere(n => n.props().id === 'facilities[1].county').props().children).toBe('N/A')
+  it('Verify facility email is rendered', () => {
+    expect(searchListComp.find('.-odd .facility_email').hostNodes().props().children).toBe('nesh_example@gmail.com')
+  })
+
+  it('Verify facility ID rendered', () => {
+    expect(searchListComp.find('.-odd .facility_id').hostNodes().props().children).toBe(193600008)
+  })
+
+  it('Verify license status rendered', () => {
+    expect(searchListComp.find('.-odd .license_status').hostNodes().props().children).toBe('Licensed')
+  })
+
+  it('Verify licensing worker rendered', () => {
+    expect(searchListComp.find('.-odd .licensing_worker').hostNodes().props().children).toBe('Kari Gutierrez')
+  })
+
+  it('Verify facility county is rendered', () => {
+    expect(searchListComp.find('.-odd .county_name').hostNodes().props().children).toBe('Los Angeles')
   })
 
   it('Verify facility type / source rendered', () => {
-    expect(searchListComp.findWhere(n => n.props().id === 'facilities[0].type').props().children).toBe('Foster Family Agency Certified Home / CWS/CMS')
-  })
-
-  it('Verify facility type / source is N/A', () => {
-    expect(searchListComp.findWhere(n => n.props().id === 'facilities[1].type').props().children).toBe('N/A / N/A')
+    expect(searchListComp.find('.-odd .facility_type_source').hostNodes().props().children).toBe('Foster Family Agency Certified Home / CWS/CMS')
   })
 
   it('Verify facility address rendered', () => {
-    expect(searchListComp.findWhere(n => n.props().id === 'facilities[0].physical_address').props().children).toBe('7 GREENROSE DRIVE, NAPA, CA 94558')
-  })
-
-  it('Verify facility address is N/A', () => {
-    expect(searchListComp.findWhere(n => n.props().id === 'facilities[1].physical_address').props().children).toBe('N/A')
+    expect(searchListComp.find('.-odd .facility_address').hostNodes().props().children).toBe('7 GREENROSE DRIVE, NAPA, CA 94558')
   })
 
   it('Verify facility phone number rendered', () => {
-    expect(searchListComp.findWhere(n => n.props().id === 'facilities[0].primary_phone').props().children).toBe('(831) 763-2735')
-  })
-
-  it('Verify facility phone number is  N/A', () => {
-    expect(searchListComp.findWhere(n => n.props().id === 'facilities[1].primary_phone').props().children).toBe('N/A')
+    expect(searchListComp.find('.-odd .phone_number').hostNodes().props().children).toBe('(831) 763-2735')
   })
 
   it('Verify facility licensee name is rendered', () => {
-    expect(searchListComp.findWhere(n => n.props().id === 'facilities[0].licensee_name').props().children).toBe('Ananya Nandi')
+    expect(searchListComp.find('.-odd .licensee_name').hostNodes().props().children).toBe('Ananya Nandi')
+  })
+
+  it('Verify facility alternative number is rendered', () => {
+    expect(searchListComp.find('.-odd .alternative_number').hostNodes().props().children).toBe('(123) 456-7891')
+  })
+
+  it('Verify facility county is N/A', () => {
+    expect(searchListComp.find('.-even .county_name').hostNodes().props().children).toBe('N/A')
+  })
+
+  it('Verify facility phone number is  N/A', () => {
+    expect(searchListComp.find('.-even .phone_number').hostNodes().props().children).toBe('N/A')
+  })
+
+  it('Verify facility type / source is N/A', () => {
+    expect(searchListComp.find('.-even .facility_type_source').hostNodes().props().children).toBe('N/A / N/A')
   })
 })
