ab282e483c9e0aef0b4f9ac6e7cb39ad4328d44c
Mohammed Nasir <Nasir2712@users.noreply.github.com>
2018-07-19 12:16:39 -0700

    Change Main Menu to Dashboard in bread crumb (#587)

--- app/javascript/components/common/breadCrumb.js
+++ app/javascript/components/common/breadCrumb.js
@@ -7,7 +7,7 @@ const BreadCrumb = ({
   <div className='container'>
     <div className='row'>
       <div className='col-xs-7'>
-        <h5>Back to: <span><a href='/dashboard'>Main Menu</a> {navigationElements.map((nav, index) => (<span key={index}> > {nav}</span>))}</span></h5>
+        <h5>Back to: <span><a href='/dashboard'>Dashboard</a> {navigationElements.map((nav, index) => (<span key={index}> > {nav}</span>))}</span></h5>
       </div>
     </div>
   </div>
