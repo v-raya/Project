4ec8cb83cc4c94233b8c8e7e4bc12db6f90dbdf0
Ratnesh Raval <ratneshraval@gmail.com>
2018-08-31 11:29:03 -0700

    Switch log level to warn in test mode (#647)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  #config.cache_store = :redis_store
+  # config.cache_store = :redis_store
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = true
@@ -41,7 +41,8 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.log_level = :warn
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
 end
